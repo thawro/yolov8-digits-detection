1b4b08ebcf96494f9be9342293ebc3ab54c909e9
thawro <tomaszhawro.kontakt@gmail.com>
2023-06-18 22:58:52 +0200

    Added instruction

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,9 +9,23 @@ const Header = () => {
             Object detection pipeline powered by &nbsp; <code className="code">onnxruntime-web</code>
         </p>
         <p>&nbsp;</p>
-        <p>
-            Instruction: draw digits on the white canvas below. The model detects digits after each line drawn.
-        </p>
+        <div className="instruction">
+            <h3>Possible options:</h3>
+            <ul>
+                <li>Draw on the white canvas below. The model detects digits after each line drawn.</li>
+                <li>Pick one of the example images (model runs after clicking on the example) and draw some extra objects</li>
+                <li>Pick one of the example videos - model runs at 15 fps</li>
+                <li>Upload your image or a video with "Upload" button</li>
+                <li>Save snapshot of the canvas with "Save snapshot" button</li>
+                <li>Record everything what happens on canvas with "Record" button. You can pause/resume the recording and save it or quit it</li>
+            </ul>
+            <br />
+            <h3>Configuration:</h3>
+            <ul>
+                <li>Control model configuration (Non Maximum Supression) with two sliders - one for confidence threshold and one for IoU threshold.</li>
+                <li>Control canvas width/height, line width and/or line color</li>
+            </ul>
+        </div>
     </div>
 }
 
